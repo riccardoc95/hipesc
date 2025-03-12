1c2ef044be7b82ffe3e235b7ac10f6196976fccb
Riccardo Ceccaroni <50733776+riccardoc95@users.noreply.github.com>
2025-03-12 16:46:39 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,8 +5,7 @@
 
 // g++ -std=c++17 -O2 -Wall -o my_program main2.cpp Complete-Striped-Smith-Waterman-Library/src/ssw_cpp.cpp -Ispoa/include -IComplete-Striped-Smith-Waterman-Library/src -Lspoa/build/lib -LComplete-Striped-Smith-Waterman-Library/src -lspoa -lComplete-Striped-Smith-Waterman-Library/src/ssw -pthread
 
-// mpic++ --std=c++17  -fopenmp -o openmp openmp.cpp -lz -lzstd -O2 lib/Striped-Smith-Waterman-Library/src/ssw_cpp.cpp -Ilib/spoa/include -Ilib/Striped-Smith-Waterman-Library/src -Llib/spoa/build/lib -Llib/Striped-Smith-Waterman-Library/src -lspoa -lssw -pthread
-
+// mpic++ --std=c++17  -fopenmp -o main main.cpp src/compression.cpp src/correction.cpp src/fastq.cpp src/paf.cpp src/utils.cpp -lz -lzstd -O2 lib/Striped-Smith-Waterman-Library/src/ssw_cpp.cpp -Ilib/spoa/include -Ilib/Striped-Smith-Waterman-Library/src -Llib/spoa/build/lib -Llib/Striped-Smith-Waterman-Library/src -lspoa -lssw -pthread
 #include <mpi.h>
 #include <omp.h>
 #include <vector>
