381b237ba2947d5bc10ea137365b440f2dde0de4
riccardo <riccardo.ceccaroni.95@gmail.com>
2025-03-13 08:40:38 +0100

    Initial Commit

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -8,6 +8,10 @@ cmake -DCMAKE_BUILD_TYPE=Release ..;
 make;
 cd ../../;
 cp Complete-Striped-Smith-Waterman-Library/src/libssw.so libssw.so
+
+
+module load openmpi/4.1.6-gcc-13.2.0-myusrbr
+
 mpic++ --std=c++17 -fopenmp -o main main.cpp -lz -lzstd -O2 Complete-Striped-Smith-Waterman-Library/src/ssw_cpp.cpp -Ispoa/include -IComplete-Striped-Smith-Waterman-Library/src -Lspoa/build/lib -LComplete-Striped-Smith-Waterman-Library/src -lspoa -lssw -pthread
 
 srun --time=15:00 --nodes=3 --ntasks=3 --cpus-per-task=16 --mem=64G --nodelist=cn1a,cn1b,cn1c ./main ../CONSENT/data/chr1/chr1.fastq ../CONSENT/data/chr1/alignment_chr1.paf
